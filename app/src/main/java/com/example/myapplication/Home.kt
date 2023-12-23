99ce7d626396f64729ca8e99cab8ef6d1ad0baa0
EvanMelon <evan89111901@gmail.com>
2023-12-24 02:52:03 +0800

    所有文章在home顯示於Logcat

--- app/src/main/java/com/example/myapplication/Home.kt
+++ app/src/main/java/com/example/myapplication/Home.kt
@@ -1,10 +1,12 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.transition.Explode
-import android.transition.Fade
-import android.view.Window
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.models.Post
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
@@ -12,9 +14,24 @@ class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        val database = Firebase.database
-        val myRef = database.getReference("message")
+//        val database = Firebase.database
+//        val myRef = database.getReference("message")
+//
+//        myRef.setValue("嗨你好")
+        val userPostsRef = Firebase.database.reference.child("posts")
+        userPostsRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val posts = dataSnapshot.children.mapNotNull { it.getValue(Post::class.java) }
+                posts.forEach { post ->
+                    Log.d("Home: post", "title: ${post.title}, body: ${post.body}, star: ${post.stars}")
+                    // 这里可以访问 post 的属性，如 post.title 和 post.body
+                }
+                // 处理帖子数量，例如更新UI
+            }
 
-        myRef.setValue("嗨你好")
+            override fun onCancelled(databaseError: DatabaseError) {
+                // 处理错误
+            }
+        })
     }
 }
